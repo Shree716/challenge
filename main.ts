b2f6792d7e25235aeab406bebc76c75af2b8c5ec
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-01-26 11:17:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     player_score += 1
-    basic.showNumber(player_score,25)
+    basic.showNumber(player_score,100)
 })
 input.onButtonPressed(Button.AB, function () {
     player_score = 0
-    basic.showNumber(player_score,25)
+    basic.showNumber(player_score,100)
 })
 input.onButtonPressed(Button.B, function () {
     player_score += -1
-    basic.showNumber(player_score,25)
+    basic.showNumber(player_score,100)
 })
 let player_score = 0
 player_score = 0
